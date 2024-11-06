248a43a5f219b846df0e046fa0da5ec9a3ce37d8
Samuu <samuelfilipevianadois@gmail.com>
2024-11-05 23:08:13 -0300

    feat: configurado rotas de usuario

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from 'cors';
+import router from "./routes";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(router)
+app.use(errorHandler)
 
 
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,3 +3,4 @@ import 'dotenv/config'
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
