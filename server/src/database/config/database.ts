c6118982f75a14cc89f9c7c060186c35ab6bd67e
Samuu <samuelfilipevianadois@gmail.com>
2024-11-20 20:36:33 -0300

    deployment changes

--- server/src/database/config/database.ts
+++ server/src/database/config/database.ts
@@ -1,12 +1,25 @@
 import 'dotenv/config'
 import { Options } from 'sequelize';
 const config: Options = {
-  username: "",
-  password: "",
-  database: "ecommercedb",  
-  host: "",  
-  dialect: "sqlite",
-  storage: "./database.sqlite",
-};
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Para forçar o uso de SSL
+      rejectUnauthorized: false, // Você pode definir isso como true se tiver um certificado SSL válido
+    },
+  },
+}
+// const config: Options = {
+//   username: "",
+//   password: "",
+//   database: "ecommercedb",  
+//   host: "",  
+//   dialect: "sqlite",
+//   storage: "./database.sqlite",
+// };
 
 export = config
